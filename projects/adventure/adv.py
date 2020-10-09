bcbf53a4c288d09652631170771fc1325ea1eddc
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-10-09 15:41:30 -0600

    all tests pass now, hit MVP with 998 moves through 500 rooms visited. just need to delete/comment out all my debugging print statements now........

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -48,7 +48,7 @@ world = World()
 # using BFS and DFT to reach every node (room in this case)
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # - shortest is 2
 # map_file = "maps/test_cross.txt"
 # - shortest is 14
@@ -56,7 +56,7 @@ map_file = "maps/test_line.txt"
 # - shortest is 14
 # map_file = "maps/test_loop_fork.txt"
 # - shortest is 24
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 # largest graph - shortest is 918
 # MUST PASS IN LESS THAN 2000 STEPS (should only take a few seconds at most tbh)
 
@@ -86,41 +86,74 @@ opp_direction = {
     "w": "e"
 }
 
-# Commands:
-# player.current_room.id,
-# player.current_room.get_exits()
-# player.travel(direction)
+# add current path if dead end
+dead_end_path = []
+
+# function to gather directions in an unexplored exit
+
+
+def unexplored_exit_directions():
+    for direction in traversal_graph[player.current_room.id]:
+        print(direction, "<-- DIRECTION")
+        if traversal_graph[player.current_room.id][direction] == "?":
+            return direction
+
+
+# step counter
+steps = 0
 
 # traverse the entire length of the graph
 while len(traversal_graph) < len(room_graph):
-    # print(player.current_room.id)
-    # print("---")
-    # print(traversal_graph, "<-- traversal graph")
-    # print("---")
-    # print(traversal_path, "<-- traversal path")
 
     # show exits in current room
     exits = player.current_room.get_exits()
+
+    # add current room and exits to traversal graph
+    if player.current_room.id not in traversal_graph:
+        traversal_graph[player.current_room.id] = {}
+        # loop thru cardinal directions in exits and add "?" to indicate they're unexplored
+        for direction in exits:
+            traversal_graph[player.current_room.id][direction] = "?"
+
+    # if coming from prev room, update current and prev room with direction opposites
+    if prev_room is not None:
+        traversal_graph[prev_room][prev_direction] = player.current_room.id
+        traversal_graph[player.current_room.id][opp_direction[prev_direction]] = prev_room
 
     # if all rooms visited, break
     if len(traversal_graph) == len(room_graph):
         break
 
-    # add current room and exits to traversal graph
-    if player.current_room.id not in traversal_graph:
-        traversal_graph[player.current_room.id] = {}
-        # loop thru cardinal directions in exits and add "?"
-        for direction in exits:
-            traversal_graph[player.current_room.id][direction] = "?"
-
     # get first unexplored room
-    for direction in traversal_graph[player.current_room.id]:
-        if traversal_graph[player.current_room.id][direction] == "?":
-            traversal_path.append(direction)
-            prev_room = player.current_room.id
-            player.travel(direction)
-            traversal_graph[prev_room][direction] = player.current_room.id
-        break
+    direction = unexplored_exit_directions()
+
+    if direction:
+        traversal_path.append(direction)
+        dead_end_path.append(opp_direction[direction])
+        prev_room = player.current_room.id
+        prev_direction = direction
+        player.travel(direction)
+        traversal_graph[prev_room][direction] = player.current_room.id
+        # increment step counter
+        steps += 1
+    # else, if no "?" in current room exits, turn around and traverse until "?" is found in new room
+    else:
+        while direction is None:
+            if len(dead_end_path) == 0:
+                break
+            # get last direction in path
+            old_direction = dead_end_path.pop()
+            traversal_path.append(old_direction)
+            player.travel(old_direction)
+            print(direction, "<< SHOULD BE OLD DIRECTION")
+            direction = unexplored_exit_directions()
+            # increment step counter
+            steps += 1
+        if len(dead_end_path) == 0:
+            dead_end_path = []
+            # then reset previous trackers
+        prev_room = None
+        prev_direction = None
 
 
 print(traversal_graph, "<-- traversal graph")
